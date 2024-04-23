2c650c25bd99df49132ed07e4f835f12a43b77b5
johnlstewart <john90stewart@protonmail.com>
2024-04-23 14:57:50 -0600

    C# Prep 1—Variables and Input/Output

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,24 @@
+// C# Prep 1—Variables and Input/Output
+// John Stewart
+// CSE 210
+// 4/23/2024
+
+// I've preserved the code already on the screen in this "Program.cs" file, just in case it's important, even if it's ok to go without it. 
+
 using System;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
     }
+
+    
 }
