1e2f52400606e723f8ea695ec544f48eedd031e2
JohnLStewart <john90stewart@protonmail.com>
2024-05-09 17:59:49 -0600

    pre-outline

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,3 +1,15 @@
+// Program must do the following
+/*
+1. Allow one to write a new entry
+2. Display the Journal contents
+3. Save the Journal to a File
+4. Load the Journal from a File
+5. provide a menu to allow option selection
+6. Cycle through 5 different prompts (or more)
+*/
+
+
+
 using System;
 
 class Program
